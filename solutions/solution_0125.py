3d10089cd51429b6fc4fd06662b53514eb6c2540
Flynn <wphestiraid@gmail.com>
2020-12-17 09:51:18 +0900

    125. Valid Palindrome
    - 리스트
    - Deque
    - 정규식 및 슬라이싱

---
+++ solutions/solution_0125.py
@@ -0,0 +1,41 @@
+import collections
+import re
+from typing import Deque
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+
+
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+
+class Solution3:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        s = re.sub(r'[^a-z0-9]', '', s)
+
+        return s == s[::-1]
