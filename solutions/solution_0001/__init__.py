16aed778b2cd02e99b332c205707d54afd640065
Flynn <wphestiraid@gmail.com>
2021-01-21 09:54:13 +0900

    0001. Two Sum 테스트 코드 주석 추가

--- solutions/solution_0001/__init__.py
+++ solutions/solution_0001/__init__.py
@@ -6,7 +6,9 @@ class Solution:
         dict_nums = {}
         for index, num in enumerate(nums):
             rest = target - num
+            # target에서 num을 뺀 숫자가 해시 테이블에 존재할 경우
             if rest in dict_nums:
                 return [dict_nums[rest], index]
+            # 그 외의 경우
             else:
                 dict_nums[num] = index
