90a288d68a6eb7ce64b03b0ee26f830e1fd780a0
Flynn <wphestiraid@gmail.com>
2021-05-24 15:28:58 +0900

    0049. Group Anagrams 문제 풀이

--- solutions/solution_0049/__init__.py
+++ solutions/solution_0049/__init__.py
@@ -1,6 +1,12 @@
+import collections
 from typing import List
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            anagrams["".join(sorted(word))].append(word)
+
+        return list(anagrams.values())
--- tests/test_0049.py
+++ tests/test_0049.py
@@ -1,11 +1,31 @@
+from typing import Any, List
 from solutions.solution_0049 import Solution
 
-
-def test_solution():
-    assert Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [
+input_values = [["eat", "tea", "tan", "ate", "nat", "bat"], [""], ["a"]]
+expected_outputs = [
+    [
         ["bat"],
         ["nat", "tan"],
         ["ate", "eat", "tea"],
-    ]
-    assert Solution().groupAnagrams([""]) == [[""]]
-    assert Solution().groupAnagrams(["a"]) == [["a"]]
+    ],
+    [[""]],
+    [["a"]],
+]
+
+
+def sort_2d_list(list_item: List[List[Any]]) -> List[List[Any]]:
+    result = []
+    for item in list_item:
+        result.append(sorted(item))
+    return sorted(result)
+
+
+def test_solution():
+    for index, input_value in enumerate(input_values):
+        actual_results = Solution().groupAnagrams(input_value)
+        actual_results = sort_2d_list(actual_results)
+
+        expected_results = sort_2d_list(expected_outputs[index])
+
+        assert len(actual_results) == len(expected_results)
+        assert actual_results == expected_results
