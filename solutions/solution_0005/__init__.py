ebfaf0e33711f1cbc319d38ea7350a1165584f57
Flynn <wphestiraid@gmail.com>
2021-06-02 13:58:37 +0900

    0005. Longest Palindromic Substring 문제 해결

--- solutions/solution_0005/__init__.py
+++ solutions/solution_0005/__init__.py
@@ -1,3 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        def check(left: int, right: int):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1 : right]
+
+        if len(s) == 1 or s == s[::-1]:
+            return s
+
+        result = ""
+        for i in range(len(s) - 1):
+            result = max(result, check(i, i + 1), check(i, i + 2), key=len)
+
+        return result
--- tests/test_0005.py
+++ tests/test_0005.py
@@ -3,20 +3,20 @@ from typing import List, TypedDict
 from solutions.solution_0005 import Solution
 
 
-class TestCase(TypedDict):
+class CaseDict(TypedDict):
     input: str
     expected: str
 
 
-test_cases: List[TestCase] = [
-    {"input": "babad", "expected": "bab"},
-    {"input": "cbbd", "expected": "bb"},
-    {"input": "a", "expected": "a"},
-    {"input": "ac", "expected": "a"},
+test_cases: List[CaseDict] = [
+    {"input": "babad", "expected": ["bab", "aba"]},
+    {"input": "cbbd", "expected": ["bb"]},
+    {"input": "a", "expected": ["a"]},
+    {"input": "ac", "expected": ["a", "c"]},
 ]
 
 
 def test_solution():
     for test_case in test_cases:
         actual_result = Solution().longestPalindrome(test_case["input"])
-        assert actual_result == test_case["expected"]
+        assert actual_result in test_case["expected"]
