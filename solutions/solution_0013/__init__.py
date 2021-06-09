c308a9093002e90a930a0e1593fef2c5b6b52f18
Flynn <wphestiraid@gmail.com>
2021-06-09 13:04:09 +0900

    0013. Roman to Integer 문제 해결

--- solutions/solution_0013/__init__.py
+++ solutions/solution_0013/__init__.py
@@ -1,3 +1,20 @@
+ROMAN_DICT = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        result = 0
+        for i in range(len(s)):
+            if i > 0 and ROMAN_DICT[s[i]] > ROMAN_DICT[s[i - 1]]:
+                result += ROMAN_DICT[s[i]] - (ROMAN_DICT[s[i - 1]] * 2)
+            else:
+                result += ROMAN_DICT[s[i]]
+        return result
