5de099c6d9305db7be2c515d2ac2e4401adf3048
Flynn <wphestiraid@gmail.com>
2021-06-09 11:17:43 +0900

    0013. Roman to Integer 문제 추가

---
+++ tests/test_0013.py
@@ -0,0 +1,22 @@
+from typing import List, TypedDict
+from solutions.solution_0013 import Solution
+
+
+class CaseDict(TypedDict):
+    input: str
+    expected: int
+
+
+test_cases: List[CaseDict] = [
+    {"input": "III", "expected": 3},
+    {"input": "IV", "expected": 4},
+    {"input": "IX", "expected": 9},
+    {"input": "LVIII", "expected": 58},
+    {"input": "MCMXCIV", "expected": 1994},
+]
+
+
+def test_solution():
+    for test_case in test_cases:
+        actual_result = Solution().romanToInt(test_case["input"])
+        assert actual_result == test_case["expected"]
