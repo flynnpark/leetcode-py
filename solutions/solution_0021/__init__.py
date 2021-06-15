26393e0536a9295607f1db4571aa4258132c2ada
Flynn <wphestiraid@gmail.com>
2021-06-15 22:16:35 +0900

    0021. Merge Two Sorted Lists 문제 해결

--- solutions/solution_0021/__init__.py
+++ solutions/solution_0021/__init__.py
@@ -4,7 +4,46 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __eq__(self, other):
+        while self and other and self.val == other.val:
+            print(self.val == other.val)
+            self = self.next
+            other = other.next
+        if not self and not other:
+            return True
+        return False
+
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        result = ListNode(None)
+        current = result
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+
+        if not l1:
+            current.next = l2
+        if not l2:
+            current.next = l1
+
+        return result.next
+
+    def mergeTwoListsRecursive(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoListsRecursive(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoListsRecursive(l1, l2.next)
+            return l2
--- tests/test_0021.py
+++ tests/test_0021.py
@@ -1,5 +1,6 @@
 from typing import List, TypedDict
-from solutions.solution_0021 import ListNode, Solution,
+from solutions.solution_0021 import ListNode, Solution
+
 
 class CaseInput(TypedDict):
     l1: ListNode
@@ -12,13 +13,32 @@ class CaseDict(TypedDict):
 
 
 test_cases: List[CaseDict] = [
-    {"input": { 'l1': [1, 2, 4], 'l2': [1, 3, 4]  }, "expected": [1,1,2,3,4,4]},
-    {"input": { 'l1': [], 'l2':  [] }, "expected": []},
-    {"input": { 'l1': [], 'l2':[0]   }, "expected": [0]},
+    {"input": {"l1": [1, 2, 4], "l2": [1, 3, 4]}, "expected": [1, 1, 2, 3, 4, 4]},
+    {"input": {"l1": [], "l2": []}, "expected": []},
+    {"input": {"l1": [], "l2": [0]}, "expected": [0]},
 ]
+
+
+def arrayToListNode(numbers: List[int]) -> ListNode:
+    result = ListNode()
+    current = result
+    for number in numbers:
+        current.next = ListNode(number)
+        current = current.next
+    return result.next
 
 
 def test_solution():
     for test_case in test_cases:
-        actual_result = Solution().mergeTwoLists(**test_case["input"])
-        assert actual_result == test_case["expected"]
+        actual_result = Solution().mergeTwoLists(
+            l1=arrayToListNode(test_case["input"]["l1"]),
+            l2=arrayToListNode(test_case["input"]["l2"]),
+        )
+        assert actual_result == arrayToListNode(test_case["expected"])
+
+    for test_case in test_cases:
+        actual_result = Solution().mergeTwoListsRecursive(
+            l1=arrayToListNode(test_case["input"]["l1"]),
+            l2=arrayToListNode(test_case["input"]["l2"]),
+        )
+        assert actual_result == arrayToListNode(test_case["expected"])
