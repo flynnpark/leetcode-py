a93c4e6581fc06587cc9fee230fedddeff4236ac
Flynn <wphestiraid@gmail.com>
2021-10-21 21:30:25 +0900

    0225. Implement Stack using Queues 문제 추가

---
+++ solutions/solution_0225/__init__.py
@@ -0,0 +1,15 @@
+class MyStack:
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def top(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
