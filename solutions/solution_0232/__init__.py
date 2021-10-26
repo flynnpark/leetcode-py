fbdc3cc7960ca726f93f2f2c3a4852c21613b400
Flynn <wphestiraid@gmail.com>
2021-10-27 08:53:55 +0900

    0232. Implement Queue using Stacks 문제 추가

---
+++ solutions/solution_0232/__init__.py
@@ -0,0 +1,15 @@
+class MyQueue:
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def peek(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
