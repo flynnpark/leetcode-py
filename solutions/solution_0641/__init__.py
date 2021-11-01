8ff250161bae0a1754fd9139e61f055cabbbf247
Flynn <wphestiraid@gmail.com>
2021-11-01 10:53:12 +0900

    0641. Design Circular Deque 문제 추가

---
+++ solutions/solution_0641/__init__.py
@@ -0,0 +1,27 @@
+class MyCircularDeque:
+    def __init__(self, k: int):
+        pass
+
+    def insertFront(self, value: int) -> bool:
+        pass
+
+    def insertLast(self, value: int) -> bool:
+        pass
+
+    def deleteFront(self) -> bool:
+        pass
+
+    def deleteLast(self) -> bool:
+        pass
+
+    def getFront(self) -> int:
+        pass
+
+    def getRear(self) -> int:
+        pass
+
+    def isEmpty(self) -> bool:
+        pass
+
+    def isFull(self) -> bool:
+        pass
