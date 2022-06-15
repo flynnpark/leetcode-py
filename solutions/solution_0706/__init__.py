291f96f015cf17160025b65a31f9c06348fa3b1b
Flynn <wphestiraid@gmail.com>
2022-06-15 18:05:14 +0900

    0706. Design HashMap 문제 1차 해결

--- solutions/solution_0706/__init__.py
+++ solutions/solution_0706/__init__.py
@@ -1,15 +1,19 @@
+import collections
+
+
 class MyHashMap:
     def __init__(self):
-        ...
+        self.table = collections.defaultdict[int, int](lambda: -1)
 
     def put(self, key: int, value: int) -> None:
-        ...
+        self.table[key] = value
 
     def get(self, key: int) -> int:
-        ...
+        return self.table[key]
 
     def remove(self, key: int) -> None:
-        ...
+        if key in self.table:
+            del self.table[key]
 
 
 # Your MyHashMap object will be instantiated and called as such:
