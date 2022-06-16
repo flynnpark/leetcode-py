167fb39782602680ec7ae4b167b9445ce626893c
Flynn <wphestiraid@gmail.com>
2022-06-16 09:31:36 +0900

    0706. Design HashMap 문제 2차 해결 및 테스트코드 작성

--- solutions/solution_0706/__init__.py
+++ solutions/solution_0706/__init__.py
@@ -1,19 +1,66 @@
 import collections
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, key: Optional[int] = None, value=None):
+        self.key: Optional[int] = key
+        self.value: Optional[int] = value
+        self.next: Optional[ListNode] = None
 
 
 class MyHashMap:
     def __init__(self):
-        self.table = collections.defaultdict[int, int](lambda: -1)
+        self.size = 1000
+        self.table = collections.defaultdict(ListNode)
+
+    def _get_index(self, key: int) -> int:
+        return key % self.size
 
     def put(self, key: int, value: int) -> None:
-        self.table[key] = value
+        index = self._get_index(key)
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
+
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+
+        p.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        return self.table[key]
+        index = self._get_index(key)
+        if self.table[index].value is None:
+            return -1
+        p = self.table[index]
+        while p:
+            if p.key == key and p.value is not None:
+                return p.value
+            p = p.next
+        return -1
 
     def remove(self, key: int) -> None:
-        if key in self.table:
-            del self.table[key]
+        index = self._get_index(key)
+        if self.table[index].value is None:
+            return
+
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = p.next if p.next else ListNode()
+            return
+
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p, p.next
 
 
 # Your MyHashMap object will be instantiated and called as such:
