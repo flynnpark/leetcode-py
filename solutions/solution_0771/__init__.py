072e20c075fce2549cc0ced00302914bc5e5c866
Flynn <wphestiraid@gmail.com>
2022-06-17 09:54:51 +0900

    0771. Jewels and Stones 문제 2차 풀이

--- solutions/solution_0771/__init__.py
+++ solutions/solution_0771/__init__.py
@@ -1,15 +1,3 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        table = {}
-        for stone in stones:
-            if stone in table:
-                table[stone] += 1
-            else:
-                table[stone] = 1
-
-        count = 0
-        for jewel in jewels:
-            if jewel in table:
-                count += table[jewel]
-
-        return count
+        return sum(stone in jewels for stone in stones)
