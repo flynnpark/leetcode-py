95018d40e0078a54cabd688c4e74acd2649d5189
Flynn <wphestiraid@gmail.com>
2022-06-20 09:35:48 +0900

    0347. Top K Frequent Elements 문제 해결

--- solutions/solution_0347/__init__.py
+++ solutions/solution_0347/__init__.py
@@ -1,3 +1,16 @@
+import collections
+import heapq
+
+
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        ...
+        freqs = collections.Counter(nums)
+        freqs_heap = []
+
+        for f in freqs:
+            heapq.heappush(freqs_heap, (-freqs[f], f))
+
+        top_k = []
+        for _ in range(k):
+            top_k.append(heapq.heappop(freqs_heap)[1])
+        return top_k
---
+++ tests/test_0347.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+
+from solutions.solution_0347 import Solution
+
+InputDict = TypedDict("InputDict", {"nums": list[int], "k": int})
+CaseDict = TypedDict("CaseDict", {"input": InputDict, "expected": list[int]})
+
+
+test_cases: list[CaseDict] = [
+    {
+        "input": {
+            "nums": [1, 1, 1, 2, 2, 3],
+            "k": 2,
+        },
+        "expected": [1, 2],
+    },
+    {
+        "input": {
+            "nums": [1],
+            "k": 1,
+        },
+        "expected": [1],
+    },
+]
+
+
+def test_solution():
+    for test_case in test_cases:
+        assert Solution().topKFrequent(**test_case["input"]) == test_case["expected"]
