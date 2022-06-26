267933c2addd39938df06d63c8b8c134b4df1323
Flynn <wphestiraid@gmail.com>
2022-06-26 17:12:54 +0900

    0200. Number of Islands 문제 해결

--- solutions/solution_0200/__init__.py
+++ solutions/solution_0200/__init__.py
@@ -1,3 +1,21 @@
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        ...
+        def dfs(i: int, j: int):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] != '1':
+                return
+
+            grid[i][j] = '0'
+
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+
+        return count
---
+++ tests/test_0200.py
@@ -0,0 +1,31 @@
+from typing import TypedDict
+
+from solutions.solution_0200 import Solution
+
+CaseDict = TypedDict("CaseDict", {"input": list[list[str]], "expected": int})
+
+test_cases: list[CaseDict] = [
+    {
+        "input": [
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+        ],
+        "expected": 1,
+    },
+    {
+        "input": [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"],
+        ],
+        "expected": 3,
+    },
+]
+
+
+def test_solution():
+    for test_case in test_cases:
+        assert Solution().numIslands(test_case["input"]) == test_case["expected"]
