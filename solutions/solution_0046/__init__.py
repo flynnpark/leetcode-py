90dc5ee10fb08036d36fe46710aa4f0910bade48
Flynn <wphestiraid@gmail.com>
2022-06-29 21:42:31 +0900

    0046. Permutations 문제 해결

--- solutions/solution_0046/__init__.py
+++ solutions/solution_0046/__init__.py
@@ -1,3 +1,18 @@
 class Solution:
     def permute(self, nums: list[int]) -> list[list[int]]:
-        ...
+        def dfs(elements: list[int]):
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+
+            for element in elements:
+                next_elements = elements[:]
+                next_elements.remove(element)
+
+                prev_elements.append(element)
+                dfs(next_elements)
+                prev_elements.pop()
+
+        results = []
+        prev_elements = []
+        dfs(nums)
+        return results
