6fe8a7c55f1dd56eae60df64f7d19e964f6378f6
Flynn <wphestiraid@gmail.com>
2023-05-09 09:13:45 +0900

    solution: 1768. Merge Strings Alternately

--- solutions/solution_1768/__init__.py
+++ solutions/solution_1768/__init__.py
@@ -1,3 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ...
+        max_length = max(len(word1), len(word2))
+        result = []
+
+        for i in range(max_length):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+
+        return ''.join(result)
