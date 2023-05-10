1156f46784dfaeb3802c54ab867a1de8faa14966
Flynn <wphestiraid@gmail.com>
2023-05-10 10:36:55 +0900

    solution: 1071. Greatest Common Divisor of Strings

--- solutions/solution_1071/__init__.py
+++ solutions/solution_1071/__init__.py
@@ -1,3 +1,9 @@
+from math import gcd
+
+
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        ...
+        if f'{str1}{str2}' != f'{str2}{str1}':
+            return ''
+
+        return str1[: gcd(len(str1), len(str2))]
