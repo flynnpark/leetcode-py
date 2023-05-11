6e8c17821d209300737f3f32fc71cfc538fde56f
Flynn <wphestiraid@gmail.com>
2023-05-12 08:59:47 +0900

    solution: 0345. Reverse Vowels of a String

--- solutions/solution_0345/__init__.py
+++ solutions/solution_0345/__init__.py
@@ -1,3 +1,19 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        ...
+        vowels = 'aeiouAEIOU'
+        left = 0
+        right = len(s) - 1
+
+        temp = list(s)
+
+        while left < right:
+            if vowels.find(temp[left]) >= 0 and vowels.find(temp[right]) >= 0:
+                temp[left], temp[right] = temp[right], temp[left]
+                left += 1
+                right -= 1
+            elif vowels.find(temp[left]) < 0:
+                left += 1
+            else:
+                right -= 1
+
+        return ''.join(temp)
