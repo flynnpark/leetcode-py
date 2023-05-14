1b35eda70a75203189d873923a7436784c9ac512
Flynn <wphestiraid@gmail.com>
2023-05-15 08:55:28 +0900

    solution: 0443. String Compression

--- solutions/solution_0443/__init__.py
+++ solutions/solution_0443/__init__.py
@@ -3,4 +3,22 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        ...
+        temp = []
+        count = 1
+        for i in range(1, len(chars)):
+            if chars[i] == chars[i - 1]:
+                count += 1
+            else:
+                temp.append((chars[i - 1], count))
+                count = 1
+        temp.append([chars[-1], count])
+
+        i = 0
+        for key, value in temp:
+            chars[i] = key
+            i += 1
+            if value > 1:
+                for num in str(value):
+                    chars[i] = num
+                    i += 1
+        return i
