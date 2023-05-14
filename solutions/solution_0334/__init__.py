c523270e31305f5a7b66a67a684cddf7d5b0db03
Flynn <wphestiraid@gmail.com>
2023-05-14 15:35:17 +0900

    solution: 0334. Increasing Triplet Subsequence

--- solutions/solution_0334/__init__.py
+++ solutions/solution_0334/__init__.py
@@ -1,6 +1,20 @@
+import sys
 from typing import List
 
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        ...
+        first = sys.maxsize
+        second = sys.maxsize
+
+        for num in nums:
+            if num > second:
+                return True
+
+            if num > first:
+                second = min(num, second)
+
+            else:
+                first = min(num, first)
+
+        return False
