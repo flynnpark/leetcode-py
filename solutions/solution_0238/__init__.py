f63e643a87b432db30354bc00c72c24b83d16e33
Flynn <wphestiraid@gmail.com>
2023-05-14 15:05:28 +0900

    solution: 0238. Product of Array Except Self

--- solutions/solution_0238/__init__.py
+++ solutions/solution_0238/__init__.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ...
+        result = []
+
+        temp = 1
+        for i in range(0, len(nums)):
+            result.append(temp)
+            temp *= nums[i]
+
+        temp = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= temp
+            temp *= nums[i]
+
+        return result
