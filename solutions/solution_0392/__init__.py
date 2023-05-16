4524d10e28bc5c1708776bfdcb2c8db89f48fb6c
Flynn <wphestiraid@gmail.com>
2023-05-16 09:11:46 +0900

    solution: 0392. Is Subsequence

--- solutions/solution_0392/__init__.py
+++ solutions/solution_0392/__init__.py
@@ -1,3 +1,9 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        ...
+        s_index = 0
+        t_index = 0
+        while s_index < len(s) and t_index < len(t):
+            if s[s_index] == t[t_index]:
+                s_index += 1
+            t_index += 1
+        return s_index == len(s)
