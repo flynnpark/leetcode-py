9cf38171d4741c3216d1df9469a129430b0ca4af
Flynn <wphestiraid@gmail.com>
2023-05-19 09:00:28 +0900

    solution: 1679. Max Number of K-Sum Pairs

--- solutions/solution_1679/__init__.py
+++ solutions/solution_1679/__init__.py
@@ -1,6 +1,20 @@
+import collections
 from typing import List
 
 
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        ...
+        result = 0
+        freq = collections.Counter(nums)
+
+        for num in nums:
+            if num in freq and freq[num] > 0:
+                freq[num] -= 1
+                diff = k - num
+                if diff in freq and freq[diff] > 0:
+                    freq[diff] -= 1
+                    result += 1
+                else:
+                    freq[num] += 1
+
+        return result
