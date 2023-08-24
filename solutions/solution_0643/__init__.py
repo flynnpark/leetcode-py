1dfdeaf55f658a5778c7482b9a56672e9cf618e2
Flynn <wphestiraid@gmail.com>
2023-08-24 09:32:12 +0900

    solution: 0643. Maximum Average Subarray I

--- solutions/solution_0643/__init__.py
+++ solutions/solution_0643/__init__.py
@@ -4,4 +4,7 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        ...
+        max_average = -sys.maxsize
+        for i in range(0, len(nums) - k + 1):
+            max_average = max(max_average, sum(nums[i : i + k]) / k)
+        return max_average
