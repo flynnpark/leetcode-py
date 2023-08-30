a5cece231569a98c4eed05165ff35732fd9cd1b9
Flynn <wphestiraid@gmail.com>
2023-08-31 08:29:15 +0900

    solution: 1732. Find the Highest Altitude

--- solutions/solution_1732/__init__.py
+++ solutions/solution_1732/__init__.py
@@ -3,4 +3,7 @@ from typing import List
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        ...
+        gain = [0] + gain
+        for i in range(1, len(gain)):
+            gain[i] += gain[i - 1]
+        return max(gain)
