91179285eb5bdcb8d172eae363070d882d5493df
Flynn <wphestiraid@gmail.com>
2024-06-18 09:41:57 +0900

    solution: 0228. Summary Ranges

--- solutions/solution_0228/__init__.py
+++ solutions/solution_0228/__init__.py
@@ -1,3 +1,14 @@
 class Solution:
     def summaryRanges(self, nums: list[int]) -> list[str]:
-        pass
+        if not nums:
+            return []
+        ranges = []
+        start = end = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] == end + 1:
+                end = nums[i]
+            else:
+                ranges.append(f'{start}->{end}' if start != end else str(start))
+                start = end = nums[i]
+        ranges.append(f'{start}->{end}' if start != end else str(start))
+        return ranges
