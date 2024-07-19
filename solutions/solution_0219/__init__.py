4bfee38d94127f585190b9e7b369e13ffed39bef
Flynn <wphestiraid@gmail.com>
2024-07-19 09:42:10 +0900

    solution: 0219. Contains Duplicate II

--- solutions/solution_0219/__init__.py
+++ solutions/solution_0219/__init__.py
@@ -1,2 +1,8 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool: ...
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        seen: dict[int, int] = {}
+        for i, num in enumerate(nums):
+            if num in seen and i - seen[num] <= k:
+                return True
+            seen[num] = i
+        return False
