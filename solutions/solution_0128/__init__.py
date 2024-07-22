46b0ed5a1c0ef1fe6cf2ebad650593b51ac70256
Flynn <wphestiraid@gmail.com>
2024-07-22 09:30:23 +0900

    solution: 0128. Longest Consecutive Sequence

--- solutions/solution_0128/__init__.py
+++ solutions/solution_0128/__init__.py
@@ -1,2 +1,17 @@
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int: ...
+    def longestConsecutive(self, nums: list[int]) -> int:
+        num_set = set(nums)
+        max_length = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_length = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_length += 1
+
+                max_length = max(max_length, current_length)
+
+        return max_length
